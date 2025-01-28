a7aa85189fd021f337fa971fc9eca6bf1a96b02d
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-01-28 23:57:05 +0300

    updates

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+DEPLOY_DIR=~/deploy
+
+
+cd $DEPLOY_DIR &&
+
+git config --global user.name "Alexey-zaliznuak" &&
+git config --global user.email "zaliznuak20@gmail.com" &&
+
+sudo git pull &&
+
+# Добавление безопасных директорий в конфигурацию Git
+sudo git config --global --add safe.directory $DEPLOY_DIR/backend &&
+sudo git config --global --add safe.directory $DEPLOY_DIR/frontend &&
+
+# Обновление подмодулей
+sudo git submodule update --init --recursive &&
+
+# Обновление backend
+cd $DEPLOY_DIR/backend &&
+sudo git fetch &&
+sudo git checkout main &&
+sudo git pull &&
+
+# Обновление frontend
+cd $DEPLOY_DIR/frontend &&
+sudo git fetch &&
+sudo git checkout master &&
+sudo git pull &&
+
+# Запуск Docker
+sudo docker compose -f $DEPLOY_DIR/docker-compose.yml up --build -d &&
+sudo docker system prune -f &&
+
+cd $DEPLOY_DIR &&
+sudo git add . &&
+sudo git commit -m "updates" &&
+sudo git push &&
+
+cd
